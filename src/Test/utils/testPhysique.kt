c14d7a3d847cff809d9451deb71d38b8d868c7b5
Noah22550 <noahdepagne@gmail.com>
2026-09-06 10:03:33 +0200

    Add Test in testPhysique.kt

--- src/Test/utils/testPhysique.kt
+++ src/Test/utils/testPhysique.kt
@@ -3,6 +3,8 @@ package Test.utils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import utils.dureeSurTerre
+import utils.longueurSurTerre
+import utils.vitesseRepereTerre
 
 class TestRelativite {
 
@@ -11,4 +13,15 @@ class TestRelativite {
         val resultat = dureeSurTerre(60.0, 299000.0)
         assertEquals(825.7444, resultat, 0.001) // 0.001  = précision requise
     }
+    @Test
+    fun `testLongueurSurTerre`() {
+        val res = longueurSurTerre(100.0, 299000.0)
+        assertEquals(7.2661, res, 0.001)
+    }
+    @Test
+    fun `testvitesseRepereTerre`() {
+        val res = vitesseRepereTerre(150000.0, 150000.0)
+        assertEquals(239933.5359, res, 0.001)
+    }
+
 }
